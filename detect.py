54320439d4e8298d91213753ba0ee223ff6ba189
Lrcuser <169140689+Lrc1109@users.noreply.github.com>
2025-05-12 16:01:23 +0800

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,15 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import RTDETR
+from cv2 import imread
+
+
+if __name__ == '__main__':
+    model = RTDETR(r'G:\lrc\RDD runs\detr\weights\best.pt') # select your model.pt path
+    model.predict(source=r'G:\lrc\RDD\1',
+                  conf=0.3,
+                  project=r'G:\lrc\RDD',
+                  name='detr',
+                  save=True,
+
+                  )
---
+++ train.py
@@ -0,0 +1,15 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import RTDETR
+
+if __name__ == '__main__':
+    model = RTDETR('ultralytics/cfg/models/rt-detr/MMS-DETR.yaml')
+    model.train(data='dataset/data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=150,
+                batch=8,
+                device='0',
+                project='runs/train',
+                name='exp',
+                )
